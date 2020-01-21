a2082f44ebb2e626415eb1198da960e8568f75a1
Karla Calderón <51063773+karlacalderon@users.noreply.github.com>
2020-01-20 21:58:04 -0800

    Starting with project

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html amp lang="es">
+  <head>
+    <meta charset="utf-8">
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+    <title>TijuanaPress.com | El portal de noticias independiente del norte del país</title>
+    <link rel="canonical" href="https://www.tijuanapress.com">
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+    <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+    <link href="https://fonts.googleapis.com/css?family=Gelasio:700|Roboto&display=swap" rel="stylesheet">
+    <style amp-custom><?php include'style.css'?></style>
+  </head>
+  <body>
+      
+      <!--MENU-MOB-->
+      <nav class="menu-mob">
+          <div class="container-menu-mob col-11">
+          <div class="element-uno">
+      <amp-img src="img/search.png" height="27" width="27"></amp-img>
+              </div>
+          
+          <div class="element-dos">
+    <amp-img src="img/logo.png" height="40" width="225"></amp-img></div>
+          
+          <div class="element-tres">
+    <amp-img src="img/menu.png" height="35" width="35"></amp-img></div>
+              </div>
+      </nav>
+      
+       <!--NOTA NUEVA-->
+      <nav class="head-mob">
+          <div class="head-img"><amp-img src="img/head.jpg" height="1200" width="800" layout="responsive"></amp-img></div>
+          
+          <div class="head-text">
+        <p class="head-auto"></p>
+        <h2>La economia social crece poco a poco</h2>
+          </div>
+          
+           <div class="head-arrow"><amp-img src="img/arrow.png" height="30" width="30" ></amp-img></div>
+      </nav>
+      
+       <!--CUADRICULA NUEVOS-->
+      
+   <div class="box-section col-11">
+   <div class="box col-12" >Noticas de hoy</div>
+       <div class="box-box">
+       <a href="#">
+       <amp-img src="img/head.jpg" height="150" width="150" layout="responsive" ></amp-img>
+       <h3>Lorem impuson nota</h3>
+    </a>  
+       </div>
+       
+       
+         <div class="box-box">
+       <a href="#">
+       <amp-img src="img/head.jpg" height="150" width="150" layout="responsive" ></amp-img>
+       <h3>Lorem impuson nota</h3>
+    </a>  
+       </div>
+       
+    </div>
+  </body>
+</html>
